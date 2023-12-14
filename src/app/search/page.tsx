711393b85e3239bad4eb79ca1e630ff9e4f0dccf
uzimaru0000 <shuji365630@gmail.com>
2023-12-15 08:59:38 +0900

    update loading for each SearchOptions

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,14 +1,23 @@
 import { getSearchProducts } from "@/usecase/product/server";
 import { Product } from "./_components/Product";
 import { Filter } from "@/usecase/product";
+import { Suspense } from "react";
+import Loading from "./loading";
 
 type Props = {
-    searchParams: Filter
-}
+  searchParams: Filter;
+};
 
 const Page: React.FC<Props> = async ({ searchParams }) => {
+  return (
+    <Suspense key={JSON.stringify(searchParams)} fallback={<Loading />}>
+      <Wrapper searchParams={searchParams} />
+    </Suspense>
+  );
+};
+
+const Wrapper: React.FC<Props> = async ({ searchParams }) => {
   const result = await getSearchProducts(searchParams);
-  console.log(result);
 
   return (
     <div className="grid grid-cols-5 gap-4">
