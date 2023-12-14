c016caca5b530d1d051caf386225a6743726763e
uzimaru0000 <shuji365630@gmail.com>
2023-12-15 08:56:10 +0900

    create loading animation

--- src/app/search/loading.tsx
+++ src/app/search/loading.tsx
@@ -1,5 +1,7 @@
 const Loading = () => {
-    return <div>Loading...</div>
-}
+  return (
+    <div className="w-16 h-16 border-gray-800 border-t-transparent border-8 rounded-full animate-spin" />
+  );
+};
 
 export default Loading;
